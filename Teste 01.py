430a191603d96c7963a87c0e504f849d437922a0
Jethro <jethrotiago@hotmail.com>
2023-07-15 20:46:47 -0300

    first commit

---
+++ Teste 01.py
@@ -0,0 +1,7 @@
+print('>>> Seja bem vindo ao Python. Preciso de algumas informações sobre você.<<<')
+print('')
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade? ')
+peso = input('Qual é o seu peso? ')
+print('Olá', nome,'. Registramos em nosso sistema a sua idade de', idade, 'anos e o seu peso de', peso, 'kgs. Tudo ok!')
+      
