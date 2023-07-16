53efe8b7441894b536d5e8f6d0c9735d47573ea5
Jethro <jethrotiago@hotmail.com>
2023-07-16 19:47:27 -0300

    Making tests

--- Teste 01.py
+++ Teste 01.py
@@ -3,5 +3,4 @@ print('')
 nome = input('Qual é o seu nome? ')
 idade = input('Qual é a sua idade? ')
 peso = input('Qual é o seu peso? ')
-print('Olá', nome,'. Registramos em nosso sistema a sua idade de', idade, 'anos e o seu peso de', peso, 'kgs. Tudo ok!')
-      
+print('Olá', nome +'. Registramos em nosso sistema a sua idade de', idade, 'anos e o seu peso de', peso, 'kgs. Tudo ok!')
