ede09f0af8c618e345d3e085cff115f2ec47218c
Jethro <jethrotiago@hotmail.com>
2023-07-17 11:18:15 -0300

    Making tests with int numbers

---
+++ Teste 03.py
@@ -0,0 +1,8 @@
+print('Teste de utilização de número inteiro')
+print('')
+n1 = int(input('Digite um número: '))
+print(type(n1))
+n2 = int(input('Digite o segundo número: '))
+print(type(n2))
+resultado = n1 + n2
+print('A soma dos dois número digitados é', resultado)
