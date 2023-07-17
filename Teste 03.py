873bff0d5f8d945e430d709f988fb26c1b36aa9a
Jethro <jethrotiago@hotmail.com>
2023-07-17 11:28:21 -0300

    Making more tests with int numbers and .formt()

--- Teste 03.py
+++ Teste 03.py
@@ -5,5 +5,6 @@ print(type(n1))
 n2 = int(input('Digite o segundo número: '))
 print(type(n2))
 resultado = n1 + n2
-print('A soma entre',n1,'e',n2,'é:',resultado)
+#print('A soma entre',n1,'e',n2,'é:',resultado)
 print('Teste de utilização de números inteiros realizada com sucesso!')
+print('A soma de {} e {} é {}'.format(n1, n2, resultado))
