81f9f04413e2198d8c03c8f355720ab881d869cc
Jethro <jethrotiago@hotmail.com>
2023-07-17 11:23:00 -0300

    Making more tests with int numbers

--- Teste 03.py
+++ Teste 03.py
@@ -5,5 +5,5 @@ print(type(n1))
 n2 = int(input('Digite o segundo número: '))
 print(type(n2))
 resultado = n1 + n2
-print('A soma das dois números digitados é', resultado)
+print('A soma entre',n1,'e',n2,'é:',resultado)
 print('Teste de utilização de números inteiros realizada com sucesso!')
