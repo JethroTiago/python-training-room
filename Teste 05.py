1646d9ad431ccb92e865aab0310d5b4a0ef91de6
Jethro <jethrotiago@hotmail.com>
2023-07-18 11:36:21 -0300

    Doing basic arithmetic operations

---
+++ Teste 05.py
@@ -0,0 +1,12 @@
+print('Exercicios aritméticos')
+print('')
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+sub = n1 - n2
+multi = n1 * n2
+divi = n1 / n2
+print('A soma dos dois números digitados é {}'.format(soma))
+print('A subtração dos valores é {}'.format(sub))
+print('A multiplicação dos números é {}'.format(multi))
+print('A divisão dos números digitados é {}'.format(divi))
