988d3cdd1470eedefa743bda86e014188ccccf42
Jethro <jethrotiago@hotmail.com>
2023-07-18 14:10:12 -0300

    Added more arithmetic tests

---
+++ Teste 06.py
@@ -0,0 +1,8 @@
+print('Exercicios aritméticos')
+print('')
+n = int(input('Digite um número: '))
+print('O número digitado foi: {}'.format(n))
+print('O dobro de {} é {}'.format(n, n*2))
+print('O triplo de {} é {}'.format(n, n*3))
+print('A raiz quadrada de {} é {}'.format(n, n**(1/2)))
+# Também poderia ser atribuido variaveis para dobro, triplo e raíz quadrada
