50edd4532a8a755f651f5c896a07ff0870986463
Jethro <jethrotiago@hotmail.com>
2023-07-19 16:02:27 -0300

    Created a temperature converter

---
+++ Teste 14.py
@@ -0,0 +1,5 @@
+print('Conversor de temperatura')
+print('')
+celsius = float(input('Digite a temperatura em ºC: '))
+fahrenheit = celsius * 1.8 + 32
+print('A temperatura de {}ºC equivale a {}ºF.'.format(celsius, fahrenheit))
