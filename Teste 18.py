6ed3bbc0cefc9daa6a19d76aac58592fb3fcd476
Jethro <jethrotiago@hotmail.com>
2023-07-21 10:45:59 -0300

    Using math library to make a Sine, Cosine and Tangent calculator

---
+++ Teste 18.py
@@ -0,0 +1,10 @@
+print('Seno, cosseno e tangente')
+print('')
+import math
+ang = float(input('Digite o valor do ângulo '))
+sen = math.sin(math.radians(ang))
+cos = math.cos(math.radians(ang))
+tan = math.tan(math.radians(ang))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(ang, sen))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(ang, cos))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(ang, tan))
