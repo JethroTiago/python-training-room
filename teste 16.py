06dc85ac0ef6b4b7d0fc0efc2bd047dbede24029
Jethro <jethrotiago@hotmail.com>
2023-07-21 10:02:00 -0300

    Created a Whole portion

---
+++ teste 16.py
@@ -0,0 +1,15 @@
+'''import math
+print ('Valor')
+print('')
+#1ª forma de fazer é com import math
+valor = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(valor, math.trunc(valor)))'''
+
+#2ª forma de fazer é com from math import trunc
+from math import trunc
+num = float(input('Digite mais um número: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
+
+'''#3ª forma de fazer é não importando nada e utilizando o int
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))'''
