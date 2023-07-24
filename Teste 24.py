af3192e22a878595e8d15579b02262c7cd742024
Jethro <jethrotiago@hotmail.com>
2023-07-24 08:34:12 -0300

    Testing a specific text - commented

--- Teste 24.py
+++ Teste 24.py
@@ -2,3 +2,4 @@ print('Nasceu em SANTO?')
 print('')
 cidade = str(input('Digite a cidade em que você nasceu: ')).strip()
 print(cidade[:5].upper() == 'Santo')
+#Digita um cidade e o programa verifica se ela começa ou não com a palavra “SANTO”.
