564d4a7fd3073bc28684011b9fe95cff32bd4414
Jethro <jethrotiago@hotmail.com>
2023-07-24 08:44:34 -0300

    Looking for a specific name within a full name

---
+++ Teste 25.py
@@ -0,0 +1,5 @@
+print('Procurando pelo nome Silva')
+print('')
+nome = str(input('Escreva seu nome completo: ')).strip()
+print('Seu nome tem SILVA? {}'.format('Silva' in nome.lower()))
+#O que foi digitado contem o nome “SILVA”?.
