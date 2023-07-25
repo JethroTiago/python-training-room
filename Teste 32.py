f09bc3690d44afc6f7dadf11899b24c4a8cef2b5
Jethro <jethrotiago@hotmail.com>
2023-07-25 16:41:44 -0300

    Leap year analyzer

---
+++ Teste 32.py
@@ -0,0 +1,12 @@
+print('Ano bissexto')
+from datetime import date
+print('-=-' * 27)
+ano = int(input('Digite um ano para saber se ele é bissexto. Digite 0 para analisar o ano atual: '))
+print('=-=' * 27)
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é bissexto!'.format(ano))
+else:
+    print('O ano {} não é bissexto!'.format(ano))
+print('FIM')
