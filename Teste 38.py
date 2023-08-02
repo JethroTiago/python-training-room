8c40bfea082821b9910944c723c31cecdedc67d1
Jethro <jethrotiago@hotmail.com>
2023-08-02 09:33:17 -0300

    Number Comparator

--- Teste 38.py
+++ Teste 38.py
@@ -3,8 +3,8 @@ print('\033[1;34m-=-\033[m' * 25)
 num1 = int(input('Digite um número inteiro: '))
 num2 = int(input('Digite outro número inteiro: '))
 if num1 > num2:
-    print('O primeiro valor digitado é MAIOR.')
+    print('O PRIMEIRO valor digitado é MAIOR.')
 elif num1 < num2:
-    print('O segundo valor digitado é MAIOR.')
+    print('O SEGUNDO valor digitado é MAIOR.')
 else:
     print('Os números digitados são iguais!')
