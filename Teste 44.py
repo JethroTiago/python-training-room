386099f49b090c2c6fb9d8623e8664314dba90bc
Jethro <jethrotiago@hotmail.com>
2023-08-03 13:43:27 -0300

    Payment Manager - Store

--- Teste 44.py
+++ Teste 44.py
@@ -9,19 +9,28 @@ print('''FORMAS DE PAGAMENTO:
 forma = int(input('Digite o valor referente a opção: '))
 if forma == 1:
     total = preco - (preco * 10) / 100
-    print('A compra de R${} vai custar R${} no final (10% de desconto).'.format(preco, total))
+    print('A compra de R${} vai custar R${:.2f} no final (10% de desconto).'.format(preco, total))
 elif forma == 2:
     total = preco - (preco * 5) / 100
-    print('A compra de R${} vai custar R${} no final (5% de desconto).'.format(preco, total))
+    print('A compra de R${} vai custar R${:.2f} no final (5% de desconto).'.format(preco, total))
 elif forma == 3:
     parcela = preco / 2
-    print('A compra será parcelada em 2x de {} e não receberá descontos.'.format(parcela))
+    print('A compra será parcelada em 2x de R${:.2f} sem juros.'.format(parcela))
 elif forma == 4:
     total = preco + (preco * 20) / 100
     quantidadeparcelas = int(input('Quantas parcelas? '))
     parcela = total / quantidadeparcelas
-    print('Sua compra será parcelada em {}x de R${} (20% de Juros).'.format(quantidadeparcelas, parcela))
+    if quantidadeparcelas == 1:
+        total = preco - (preco * 5) / 100
+        print('A compra à vista no cartão vai custar R${:.2f} no final (5% de desconto).'.format(total))
+    elif quantidadeparcelas == 2:
+        parcela = preco / 2
+        print('A compra será parcelada em 2x de R${:.2f} sem juros.'.format(parcela))
+    elif quantidadeparcelas <= 12:
+        print('Sua compra será parcelada em {}x de R${:.2f} (20% de Juros).'.format(quantidadeparcelas, parcela))
+    else:
+        print('\033[1:31mA compra só pode ser dividida em até 12x. Tente novamente\033[m.')
 else:
-    print('O valor digitado não é válido. Tente novamente.')
+    print('\033[1:31mO valor digitado não é válido. Tente novamente\033[m.')
 print('\033[1:36m{:=^44}\033[m'.format(' LOJAS PYTHON '))
 print('FIM')
