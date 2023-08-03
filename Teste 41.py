f0f7991c5075aae56b4bd2e56d7d716d64794253
Jethro <jethrotiago@hotmail.com>
2023-08-03 10:22:50 -0300

    Ranking athletes

--- Teste 41.py
+++ Teste 41.py
@@ -4,15 +4,16 @@ print('\033[1;31m-=-\033[m' * 25)
 ano = int(input('Digite o ano de nascimento do atleta: '))
 atual = date.today().year
 idade = atual - ano
-print('O atleta tem {} anos.'.format(idade))
+print('O atleta tem \033[1:32m{}\033[m anos.'.format(idade))
 if idade <= 9:
     print('Classificação: \033[1:34mMIRIM\033[m')
-elif idade <= 14 and idade > 9:
+elif idade <= 14:
     print('Classificação: \033[1:34mINFANTIL\033[m')
-elif idade <= 19 and idade > 14:
+elif idade <= 19:
     print('Classificação: \033[1:34mJÚNIOR\033[m')
-elif idade <= 25 and idade > 19:
+elif idade <= 25:
     print('Classificação: \033[1:34mSÊNIOR\033[m')
 else:
-    print('Classificação: MASTER')
+    print('Classificação: \033[1:34mMASTER\033[m')
+print('\033[1;31m-=-\033[m' * 25)
 print('FIM')
