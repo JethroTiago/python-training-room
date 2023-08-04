ff48487090dc22a3531c723cdd72385fc8ff2053
Jethro <jethrotiago@hotmail.com>
2023-08-04 13:35:56 -0300

    Pair count - 2 to 50

---
+++ Teste 47.py
@@ -0,0 +1,7 @@
+print('\033[1;35mContagem de pares\033[m')
+print('\033[1;33m-=-\033[m' * 24)
+for c in range(2, 51, 2):
+    print(c, end=' ')
+print('')
+print('\033[1;33m-=-\033[m' * 24)
+print('FIM')
