b252e428af77299538a77ab52b25ef093ac3baac
Jethro <jethrotiago@hotmail.com>
2023-08-07 09:08:49 -0300

    Add odd multiples of 3

--- Teste 48.py
+++ Teste 48.py
@@ -1,1 +1,11 @@
-print('Python test 48')
+print('\033[1;36mSoma ímpares multiplos de 3\033[m')
+print('\033[1;33m-=-\033[m' * 24)
+cont = 0
+soma = 0
+for c in range (3, 501, 2):
+    if c % 3 == 0:
+        cont += 1 #contador (cont = cont + 1)
+        soma += c #acumulador (soma = soma + c
+        print(c)
+print('\033[1;33m-=-\033[m' * 24)
+print('A soma dos {} valores acima é {}.'.format(cont, soma))
