100947756088db1f7ff7115552c93d972d965a77
Jethro <jethrotiago@hotmail.com>
2023-08-07 13:59:15 -0300

    Arithmetic progression

--- Teste 51.py
+++ Teste 51.py
@@ -1,2 +1,9 @@
-print('\033[1;35mDez termos de um PA\033[m')
-print('\033[1;34m-=-\033[m' * 24)
+print('\033[1;34m-=-\033[m' * 15)
+print('\033[1;35mDEZ TERMOS DE UMA PROGRESSÃO ARITMÉTICA\033[m')
+print('\033[1;34m-=-\033[m' * 15)
+termo = int(input('Digite o primeiro TERMO: '))
+razao = int(input('Digite a RAZÃO: '))
+decimo = termo + (11 - 1) * razao
+for c in range(termo, decimo, razao):
+    print('{}' .format(c), end =' => ')
+print('FIM')
