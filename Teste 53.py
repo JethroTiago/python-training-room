7aabe50372c75da51e696a8fa80303c9dc7bf50b
Jethro <jethrotiago@hotmail.com>
2023-08-07 18:25:49 -0300

    Palindrome detector

--- Teste 53.py
+++ Teste 53.py
@@ -6,9 +6,9 @@ print('\033[1;34m-=-\033[m' * 15)
 frase = str(input('\033[1mDigite uma palavra/frase:\033[m ')).strip().upper() #retirado espaços e colocado em maiúsculas
 palavras = frase.split() #separa cada palavra digitada
 junto = ''.join(palavras) #une as palavras digitadas
-inverso = ''
-for letras in range(len(junto) - 1, -1, -1): #inverte as letras do fim para o inicio
-    inverso += junto[letras]
+inverso = junto[::-1] #inverte as letras do fim para o início
+'''for letras in range(len(junto) - 1, -1, -1): #inverte as letras do fim para o início
+    inverso += junto[letras]'''
 print('O inverso da palavra/frase \033[1;36m{}\033[m é \033[1;36m{}\033[m.'.format(junto, inverso))
 if inverso == junto:
     print('Temos um \033[1;32mPALÍNDROMO\033[m!')
