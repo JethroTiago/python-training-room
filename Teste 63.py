2807d7939e1204952e5985a7f2fcdecd16be260d
Jethro <jethrotiago@hotmail.com>
2023-08-13 10:18:18 -0300

    Fibonacci sequence V1.0

--- Teste 63.py
+++ Teste 63.py
@@ -1,3 +1,16 @@
 print('\033[1;34m-=-\033[m' * 15)
 print('\033[1;33mSEQUÊNCIA DE FIBONACCI V1.0\033[m')
 print('\033[1;34m-=-\033[m' * 15)
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('{} --> {} '.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print('--> {} '.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print('--> FIM')
+print('\033[1;34m-=-\033[m' * 15)
