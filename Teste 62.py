e84ab9b33e4bb2ea3f74c56a8a1f74900ed67362
Jethro <jethrotiago@hotmail.com>
2023-08-12 22:05:30 -0300

    Creating the SUPER Arithmetic progression

--- Teste 62.py
+++ Teste 62.py
@@ -1,3 +1,13 @@
 print('\033[1;34m-=-\033[m' * 15)
 print('\033[1;31mSUPER PROGRESSÃO ARITMÉTICA\033[m')
 print('\033[1;34m-=-\033[m' * 15)
+primeiro = int(input('Digite o primeiro \033[1;35mTERMO\033[m: '))
+razao = int(input('Digite a \033[1;36mRAZÃO\033[m: '))
+termo = primeiro
+contador = 1
+while contador <= 10:
+    print('{} \033[1;32m->\033[m '.format(termo), end='')
+    termo += razao
+    contador += 1
+print('\033[1;31mFIM\033[m')
+print('\033[1;34m-=-\033[m' * 15)
