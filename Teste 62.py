8540384285a04c803dbb54406bac1df11c5f1e48
Jethro <jethrotiago@hotmail.com>
2023-08-12 22:43:17 -0300

    SUPER Arithmetic progression

--- Teste 62.py
+++ Teste 62.py
@@ -5,9 +5,17 @@ primeiro = int(input('Digite o primeiro \033[1;35mTERMO\033[m: '))
 razao = int(input('Digite a \033[1;36mRAZÃO\033[m: '))
 termo = primeiro
 contador = 1
-while contador <= 10:
-    print('{} \033[1;32m->\033[m '.format(termo), end='')
-    termo += razao
-    contador += 1
-print('\033[1;31mFIM\033[m')
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while contador <= total:
+        print('{} \033[1;32m->\033[m '.format(termo), end='')
+        termo += razao
+        contador += 1
+    print('Pausa.')
+    mais = int(input('Digite quantos termos você quer mostrar a mais: '))
 print('\033[1;34m-=-\033[m' * 15)
+print('Progressão finalizada com {} \033[1;35mTERMOS\033[m exibidos.'.format(total))
+print('\033[1;34m-=-\033[m' * 15)
+print('FIM')
