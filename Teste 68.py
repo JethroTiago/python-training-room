f8e9711afda15aafcc7dc348a539077d7f093fb2
Jethro <jethrotiago@hotmail.com>
2023-08-16 09:45:29 -0300

    Even or Odd Game

--- Teste 68.py
+++ Teste 68.py
@@ -1,3 +1,31 @@
-print('\033[1;35m-=-\033[m' * 15)
-print('\033[1;34mJOGO DO PAR OU ÍMPAR\033[m')
-print('\033[1;35m-=-\033[m' * 15)
+from random import randint
+print('\033[1;36m-=-\033[m' * 15)
+print('\033[1;31mJOGO DO PAR OU ÍMPAR\033[m')
+print('\033[1;36m-=-\033[m' * 15)
+print('Vamos jogar PAR ou ÍMPAR!')
+print('=-=' * 15)
+vitoria = 0
+while True:
+    player = int(input('ENTER YOUR NUMBER: '))
+    computer = randint(0, 10)
+    total = player + computer
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('PAR ou ÍMPAR? [P/I]: ')).strip().upper()[0]
+    print(f'Você jogou {player} e o computador jogou {computer}. O TOTAL foi {total}')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print(f'-> {total} é PAR! Você Ganhou!')
+            vitoria += 1
+        else:
+            print(f'-> {total} é ÍMPAR! Você Perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print(f'-> {total} é ÍMPAR! Você Ganhou!')
+            vitoria += 1
+        else:
+            print(f'-> {total} é PAR! Você Perdeu!')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER! Você venceu {vitoria} vezes!')
