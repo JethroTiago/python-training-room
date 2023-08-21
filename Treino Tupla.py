7ddac6d447693bcf9b9bfd108822ae4656eeced3
Jethro <jethrotiago@hotmail.com>
2023-08-20 21:58:41 -0300

    Tuple training

---
+++ Treino Tupla.py
@@ -0,0 +1,6 @@
+print('\033[1;36m-=-\033[m' * 15)
+print('\033[1;31mTREINO DE TUPLA\033[m')
+print('\033[1;36m-=-\033[m' * 15)
+lanches = ('Suco', 'Hamburguer', 'Batata-Frita', 'Coca-Cola')
+for comida in range(0, len(lanches)):
+    print(f'Eu comi {lanches[comida]}')
