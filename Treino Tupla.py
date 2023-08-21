c3663c3b3e2ca6d9a27cf7821df0f3c6b5a982ab
Jethro <jethrotiago@hotmail.com>
2023-08-20 22:03:25 -0300

    Tuple training

--- Treino Tupla.py
+++ Treino Tupla.py
@@ -2,5 +2,7 @@ print('\033[1;36m-=-\033[m' * 15)
 print('\033[1;31mTREINO DE TUPLA\033[m')
 print('\033[1;36m-=-\033[m' * 15)
 lanches = ('Suco', 'Hamburguer', 'Batata-Frita', 'Coca-Cola')
-for comida in range(0, len(lanches)):
-    print(f'Eu comi {lanches[comida]}')
+#for comida in range(0, len(lanches)):
+#    print(f'Eu comi {lanches[comida]}')
+for pos, comida in enumerate(lanches):
+    print(f'Eu vou comer {comida} na posição {pos}')
