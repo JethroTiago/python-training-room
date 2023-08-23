4354ca5d04769fd1d64d437b29a479d3cafafccc
Jethro <jethrotiago@hotmail.com>
2023-08-23 17:08:09 -0300

    Added Teste 72

--- Treino Tupla.py
+++ Treino Tupla.py
@@ -4,5 +4,7 @@ print('\033[1;36m-=-\033[m' * 15)
 lanches = ('Suco', 'Hamburguer', 'Batata-Frita', 'Coca-Cola')
 #for comida in range(0, len(lanches)):
 #    print(f'Eu comi {lanches[comida]}')
-for pos, comida in enumerate(lanches):
-    print(f'Eu vou comer {comida} na posição {pos}')
+#for pos, comida in enumerate(lanches):
+#   print(f'Eu vou comer {comida} na posição {pos}')
+print(sorted(lanches))
+print(lanches)
