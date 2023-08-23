1153735dd8c37c69cf2e79344907e14c69519e51
Jethro <jethrotiago@hotmail.com>
2023-08-23 19:25:59 -0300

    Added Teste 73

---
+++ Teste 73.py
@@ -0,0 +1,7 @@
+print('\033[1;35m-=-\033[m' * 15)
+print('\033[1;32mBRASILEIRÃO - RODADA 20\033[m')
+print('\033[1;35m-=-\033[m' * 15)
+times = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fluminense', 'Grêmio',
+         'Atletico-PR', 'Bragantino', 'Fortaleza', 'Cuiabá', 'São Paulo',
+         'Atlético-MG', 'Cruzeiro', 'Corinthians', 'Internacional', 'Goiás',
+         'Bahia', 'Santos', 'Vasco da Gama', 'Coritiba', 'America-MG')
