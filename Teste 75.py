3ee3611f862051f81bf51a4fa5977201638bc46e
Jethro <jethrotiago@hotmail.com>
2023-08-25 12:05:13 -0300

    Data analysis in a Tuple

--- Teste 75.py
+++ Teste 75.py
@@ -1,5 +1,5 @@
 print('\033[1;34m=-=\033[m' * 15)
-print('\033[1;33mANALISE DE DADOS EM UMA TUPLA\033[m')
+print('\033[1;33mANÁLISE DE DADOS EM UMA TUPLA\033[m')
 print('\033[1;34m=-=\033[m' * 15)
 num = (int(input('Digite um número: ')),
        int(input('Digite outro número: ')),
