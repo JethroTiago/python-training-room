6004ee8770cc32a2cb77d82055ec3d20c735276a
Jethro <jethrotiago@hotmail.com>
2023-08-27 19:38:43 -0300

    Counting vowels in tuples

--- Teste 77.py
+++ Teste 77.py
@@ -1,3 +1,11 @@
 print('\033[1;34m=-=\033[m' * 15)
 print('\033[1;33mCONTANDO VOGAIS EM TUPLA\033[m')
 print('\033[1;34m=-=\033[m' * 15)
+palavras = ('aprender', 'programar', 'linguagem', 'Python',
+            'curso', 'grátis', 'estudar', 'praticar',
+            'trabalhar', 'mercado', 'programador', 'futuro')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos as VOGAIS ',end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
